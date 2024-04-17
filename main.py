fc2d387e6a125c33bbaab7859ac87b8841034316
rebecafmatias <rebecafeitosamatias@gmail.com>
2024-04-17 20:51:33 -0300

    salvando o arquivo main novamente

---
+++ main.py
@@ -0,0 +1,18 @@
+print('Hello World!')
+
+# Variáveis
+idade = 26
+nome = 'Rebeca'
+
+#Print
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_revertida = lista.reverse()
